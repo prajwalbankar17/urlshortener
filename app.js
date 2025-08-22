4db2a27ac024ee615f578a18cb69b7b8b51c214d
Prajwal <prajwalbankar11@gmail.com>
2025-08-22 17:05:34 +0530

    solve the deployment issye

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ import { createServer } from "http";
 import path from "path";
 import crypto from "crypto";
 
-const PORT = 3001;
+// const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const DATA_FILE = path.join("data", "links.json");
 
 const serveFile = async (res, filePath, contentType) => {
@@ -100,6 +101,11 @@ const server = createServer(async (req, res) => {
   }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// server.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
