e4c17a0f1d79a573cd85bdc635eee4ff6955bddd
Prajwal <prajwalbankar11@gmail.com>
2025-08-22 17:41:17 +0530

    N/A

--- app.js
+++ app.js
@@ -1,3 +1,117 @@
+// //create an server using htmlmcss and nodejs
+
+// import { readFile, writeFile } from "fs/promises";
+// import { createServer } from "http";
+// import path from "path";
+// import crypto from "crypto";
+
+// // const PORT = 3001;
+// const PORT = process.env.PORT || 3001;
+// const DATA_FILE = path.join("data", "links.json");
+
+// const serveFile = async (res, filePath, contentType) => {
+//   try {
+//     const data = await readFile(filePath);
+//     res.writeHead(200, { "Content-Type": contentType });
+//     res.end(data);
+//   } catch (error) {
+//     res.writeHead(404, { "Content-Type": contentType });
+//     res.end("404 page not Found");
+//   }
+// };
+
+// const loadLinks = async () => {
+//   try {
+//     const data = await readFile(DATA_FILE, "utf-8");
+//     return JSON.parse(data);
+//   } catch (error) {
+//     if (error.code === "ENOENT") {
+//       await writeFile(DATA_FILE, JSON.stringify({}));
+//       return {};
+//     }
+//     throw error;
+//   }
+// };
+
+// const server = createServer(async (req, res) => {
+//   console.log(req.url);
+
+//   if (req.method === "GET") {
+//     if (req.url === "/") {
+//       return serveFile(res, path.join("public", "index.html"), "text/html");
+//     } else if (req.url === "/style.css") {
+//       return serveFile(res, path.join("public", "style.css"), "text/css");
+//     } else if (req.url === "/links") {
+//       const links = await loadLinks();
+//       res.writeHead(200, { "Content-Type": "application/json" });
+//       return res.end(JSON.stringify(links));
+//     } else {
+//       const links = await loadLinks();
+//       const shortCode = req.url.slice(1);
+//       console.log("links red.", req.url);
+//       if (links[shortCode]) {
+//         res.writeHead(302, { Location: links[shortCode] });
+//         return res.end();
+//       }
+//       res.writeHead(404, { "Content-Type": "text/plain" });
+//       return res.end("Short URL not found");
+//     }
+//   } 
+
+//   if (req.method === "POST" && req.url === "/shorten") {
+//     const links = await loadLinks();
+
+//     let body = "";
+//     req.on("data", (chunk) => (body += chunk));
+//     req.on("end", async () => {
+//       try {
+//         const { url, shortCode } = JSON.parse(body);
+
+//         if (!url) {
+//           res.writeHead(400, { "Content-Type": "text/plain" });
+//           return res.end("URL is required");
+//         }
+
+//         const finalShortCode =
+//           shortCode || crypto.randomBytes(4).toString("hex");
+
+//         if (links[finalShortCode]) {
+//           res.writeHead(400, { "Content-Type": "text/plain" });
+//           return res.end(
+//             "Short code URL already exists. Please choose another."
+//           );
+//         }
+
+//         links[finalShortCode] = url;
+//         await saveLinks(links);
+
+//         res.writeHead(200, { "Content-Type": "application/json" });
+//         res.end(JSON.stringify({ success: true, shortUrl: finalShortCode }));
+//       } catch (err) {
+//         res.writeHead(400, { "Content-Type": "text/plain" });
+//         res.end("Invalid request body");
+//       }
+//     });
+
+//     // Add this to handle errors if the request is aborted
+//     req.on("error", () => {
+//       res.writeHead(500, { "Content-Type": "text/plain" });
+//       res.end("Server error");
+//     });
+//   }
+// });
+
+// // server.listen(PORT, () => {
+// //   console.log(`Server is running on http://localhost:${PORT}`);
+// // });
+
+
+// server.listen(PORT, "0.0.0.0", () => {
+//   console.log(`Server is running on http://0.0.0.0:${PORT}`);
+// });
+
+
+
 //create an server using htmlmcss and nodejs
 
 import { readFile, writeFile } from "fs/promises";
@@ -5,7 +119,6 @@ import { createServer } from "http";
 import path from "path";
 import crypto from "crypto";
 
-// const PORT = 3001;
 const PORT = process.env.PORT || 3001;
 const DATA_FILE = path.join("data", "links.json");
 
@@ -31,6 +144,11 @@ const loadLinks = async () => {
     }
     throw error;
   }
+};
+
+// Added missing saveLinks function
+const saveLinks = async (links) => {
+  await writeFile(DATA_FILE, JSON.stringify(links, null, 2));
 };
 
 const server = createServer(async (req, res) => {
@@ -100,11 +218,6 @@ const server = createServer(async (req, res) => {
     });
   }
 });
-
-// server.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
 
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://0.0.0.0:${PORT}`);
